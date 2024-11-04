c15dee125c63a33f6d7b8dacf5af269cf8f1b05d
MR-Alyousif <mryousif967@gmail.com>
2024-11-04 11:59:17 +0300

    Create DoctorPage component with welcome message and prescription form

---
+++ app/doctor/page.tsx
@@ -0,0 +1,15 @@
+import Welcome from "@/components/welcome";
+import { PrescriptionForm } from "@/components/forms/prescription-form";
+
+export default function DoctorPage() {
+  return (
+    <div className="grid grid-rows-[1fr] items-center justify-items-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <Welcome
+        primaryText="Doctor&#39;s Prescription Dashboard"
+        secondaryText="Fill out the patient&#39;s prescription details below."
+      />
+
+      <PrescriptionForm />
+    </div>
+  );
+}
