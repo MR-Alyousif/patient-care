f8d7bc0b0ecabf9470704263ab548288438c041a
MR-Alyousif <mryousif967@gmail.com>
2024-11-10 05:39:59 +0300

    Refactor PatientForm to conditionally render Order component

--- components/forms/patient-form.tsx
+++ components/forms/patient-form.tsx
@@ -21,6 +21,7 @@ import {
 import { AnimatedSubscribeButton } from "../ui/animated-subscribe-button";
 import { NeonGradientCard } from "../ui/neon-gradient-card";
 import { CheckIcon, ChevronRightIcon } from "lucide-react";
+import { Order } from "../order";
 
 const formSchema = z.object({
   patientId: z
@@ -52,78 +53,83 @@ export function PatientForm() {
   }
 
   return (
-    <NeonGradientCard
-      neonColors={{ firstColor: "#0f2f76", secondColor: "#81d4fa" }}
-      className="max-w-md items-center justify-center text-center space-y-6"
-    >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="patientId"
-            render={({ field }) => (
-              <FormItem className="flex flex-col items-center">
-                <FormLabel className="text-[#0f2f76]">Patient ID</FormLabel>
-                <FormControl>
-                  <InputOTP maxLength={10} {...field}>
-                    <InputOTPGroup>
-                      {Array.from({ length: 10 }).map((_, index) => (
-                        <InputOTPSlot key={index} index={index} />
-                      ))}
-                    </InputOTPGroup>
-                  </InputOTP>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div>
+      {!submitStatus && (
+        <NeonGradientCard
+          neonColors={{ firstColor: "#0f2f76", secondColor: "#81d4fa" }}
+          className="max-w-md items-center justify-center text-center space-y-6"
+        >
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="patientId"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col items-center">
+                    <FormLabel className="text-[#0f2f76]">Patient ID</FormLabel>
+                    <FormControl>
+                      <InputOTP maxLength={10} {...field}>
+                        <InputOTPGroup>
+                          {Array.from({ length: 10 }).map((_, index) => (
+                            <InputOTPSlot key={index} index={index} />
+                          ))}
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <FormField
-            control={form.control}
-            name="prescriptionNumber"
-            render={({ field }) => (
-              <FormItem className="flex flex-col items-center">
-                <FormLabel className="text-[#0f2f76]">
-                  Prescription Number
-                </FormLabel>
-                <FormControl>
-                  <InputOTP maxLength={7} {...field}>
-                    <InputOTPGroup>
-                      <InputOTPSlot index={0} />
-                    </InputOTPGroup>
-                    <InputOTPSeparator />
-                    <InputOTPGroup>
-                      {Array.from({ length: 6 }).map((_, index) => (
-                        <InputOTPSlot key={index + 1} index={index + 1} />
-                      ))}
-                    </InputOTPGroup>
-                  </InputOTP>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="flex justify-center w-full">
-            <AnimatedSubscribeButton
-              buttonColor="#0f2f76"
-              buttonTextColor="#ffffff"
-              subscribeStatus={submitStatus}
-              initialText={
-                <span className="group inline-flex items-center">
-                  Submit
-                  <ChevronRightIcon className="ml-1 size-4 transition-transform duration-300 group-hover:translate-x-1" />
-                </span>
-              }
-              changeText={
-                <span className="group inline-flex items-center">
-                  <CheckIcon className="mr-2 size-4" />
-                  Submitted
-                </span>
-              }
-            />
-          </div>
-        </form>
-      </Form>
-    </NeonGradientCard>
+              <FormField
+                control={form.control}
+                name="prescriptionNumber"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col items-center">
+                    <FormLabel className="text-[#0f2f76]">
+                      Prescription Number
+                    </FormLabel>
+                    <FormControl>
+                      <InputOTP maxLength={7} {...field}>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          {Array.from({ length: 6 }).map((_, index) => (
+                            <InputOTPSlot key={index + 1} index={index + 1} />
+                          ))}
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-center w-full">
+                <AnimatedSubscribeButton
+                  buttonColor="#0f2f76"
+                  buttonTextColor="#ffffff"
+                  subscribeStatus={submitStatus}
+                  initialText={
+                    <span className="group inline-flex items-center">
+                      Submit
+                      <ChevronRightIcon className="ml-1 size-4 transition-transform duration-300 group-hover:translate-x-1" />
+                    </span>
+                  }
+                  changeText={
+                    <span className="group inline-flex items-center">
+                      <CheckIcon className="mr-2 size-4" />
+                      Submitted
+                    </span>
+                  }
+                />
+              </div>
+            </form>
+          </Form>
+        </NeonGradientCard>
+      )}
+      {submitStatus && <Order />}
+    </div>
   );
 }
