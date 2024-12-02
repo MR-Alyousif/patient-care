e6e616c8a738c4bdc2627a51a55e2eee2dd298c0
MR-Alyousif <mryousif967@gmail.com>
2024-12-03 00:19:23 +0300

    Update CentralScreen to use Ticket type for currentTicket state

--- components/CentralScreen.tsx
+++ components/CentralScreen.tsx
@@ -1,9 +1,9 @@
-
 import { useEffect, useState } from 'react';
-import { Connector } from 'rticonnextdds-connector';
+import Connector from 'rticonnextdds-connector';
+import { Ticket } from '../types/ticket';
 
 export function CentralScreen() {
-  const [currentTicket, setCurrentTicket] = useState(null);
+  const [currentTicket, setCurrentTicket] = useState<Ticket | null>(null);
 
   useEffect(() => {
     const connector = new Connector('MyParticipantLibrary::Zero', 'DDS.xml');
